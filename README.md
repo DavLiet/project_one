6c191a115b1923c9c9f75f7c2c3ec9a8bb5197bf
sarahlink <selink@wustl.edu>
2018-09-03 21:45:04 -0500

    added Firebase functionality

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # project_one
 This is our code for Project 1 for CSE 437 Software Engineering Workshop
+
+Firebase documentation: https://firebase.google.com/docs/database/web/read-and-write?authuser=0
--- script.js
+++ script.js
@@ -2,13 +2,41 @@ var x;
 var y;
 var gameBoard = document.getElementById("board");
 var hits = 0;
-var highScore = 0; //get this from database or w/e
+var highScore = 0;
+var person;
 
+
+
+/**
+ * Prompts user for username and loads their previous scores from firebase
+ */
+function init(){
+    person = prompt("Enter a name to save your game", "");
+
+    if (person == null || person == "") { //nothing entered in prompt
+        person = null;
+        alert("You can still play, but your high score won't be saved.")
+    } else {
+    //grab user's high score
+    firebase.database().ref(person).once('value').then(function(snapshot) {
+      var scoreFromDatabase = (snapshot.val());
+        //set highscore in the div
+        if (scoreFromDatabase == null) { //nothing in DB for this player
+             document.getElementById("hScore").innerHTML = 0;
+        } else {
+            document.getElementById("hScore").innerHTML = scoreFromDatabase.score;
+        }
+        
+    });
+}
+//get the game goin'
+randomRectangle();
+}
 
 /**
  * Calculates random x-coordinate
  */
-function randomX(){
+ function randomX(){
     let x = Math.floor((Math.random()*450));    
     return x;
 }
@@ -16,7 +44,7 @@ function randomX(){
 /**
  * Calculates random y-coordinate
  */
-function randomY(){
+ function randomY(){
     let y = Math.floor((Math.random()*450));
     return y;
 }
@@ -24,12 +52,12 @@ function randomY(){
 /**
  * Draws random rectangle
  */
-function randomRectangle(){
+ function randomRectangle(){
     let rectangle = gameBoard.getContext("2d");
     rectangle.clearRect(0,0,500,500);
     rectangle.beginPath();
-     x = randomX();
-     y = randomY();
+    x = randomX();
+    y = randomY();
     rectangle.rect(x,y,50,50);
     rectangle.fillStyle="#000000";
     rectangle.fill();
@@ -46,10 +74,18 @@ gameBoard.addEventListener('click',(event)=>{
     }
     else{
         alert("You missed! Score: " + hits);
-        if (hits > highScore) {
-            //save highscore in database
+        if (person != null) { //if logged in
+            if (hits > highScore) { //if new highscore
+                highScore = hits;
+                console.log("saving new score");
+                //set new highscore in database
+                firebase.database().ref(person).set({
+                    score: hits,
+                });
+                document.getElementById("hScore").innerHTML = hits;
+            }
         }
-        hits = 0;
+        hits = 0; //reset hits
+        document.getElementById("score").innerHTML = 0;
     }
 });
-
