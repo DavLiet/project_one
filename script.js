a9832586eac3974e79431c49a0cb61549e5e3078
sarahlink <selink@wustl.edu>
2018-09-04 13:18:37 -0500

    created classes

--- script.js
+++ script.js
@@ -1,85 +1,83 @@
-var x;
-var y;
+
 var gameBoard = document.getElementById("board");
 var hits = 0;
+
+class Rectangle {
+
+    constructor() {
+        this.x = this.genRandomCoord();
+        this.y = this.genRandomCoord();
+        let board = gameBoard.getContext("2d");
+        board.clearRect(0,0,500,500);
+        board.beginPath();
+        board.rect(this.x,this.y,50,50);
+        board.fillStyle="#000000";
+        board.fill();
+        board.closePath();
+    }
+
+    genRandomCoord() {
+        return Math.floor((Math.random()*450));
+    }
+
+};
 var highScore = 0;
-var person;
 
+class User {
 
+    constructor() {
+        this.name = prompt("Enter a name to save your game", "");
+        if (this.name == null || this.name == "") { //nothing entered in prompt
+            this.name = null;
+            alert("You can still play, but your high score won't be saved.")
+        } else {
+            this.setHighScore();
+            
+        }
+    }
+    setHighScore() {
+        firebase.database().ref(this.name).once('value').then(function(snapshot) {
+            if (snapshot.val() != null) {
+                console.log(snapshot.val().score);
+                highScore = snapshot.val().score;
+            }
+            console.log(snapshot.val());
+            console.log(highScore);
+            document.getElementById("hScore").innerHTML = highScore;
+        });
+    }
+};
+
+var rect;
+var player;
 
 /**
  * Prompts user for username and loads their previous scores from firebase
  */
-function init(){
-    person = prompt("Enter a name to save your game", "");
-
-    if (person == null || person == "") { //nothing entered in prompt
-        person = null;
-        alert("You can still play, but your high score won't be saved.")
-    } else {
-    //grab user's high score
-    firebase.database().ref(person).once('value').then(function(snapshot) {
-      var scoreFromDatabase = (snapshot.val());
-        //set highscore in the div
-        if (scoreFromDatabase == null) { //nothing in DB for this player
-             document.getElementById("hScore").innerHTML = 0;
-        } else {
-            document.getElementById("hScore").innerHTML = scoreFromDatabase.score;
-        }
-        
-    });
-}
-//get the game goin'
-randomRectangle();
+ function init(){
+    rect = new Rectangle();
+    player = new User();
+rect = new Rectangle();
 }
 
-/**
- * Calculates random x-coordinate
- */
- function randomX(){
-    let x = Math.floor((Math.random()*450));    
-    return x;
-}
-
-/**
- * Calculates random y-coordinate
- */
- function randomY(){
-    let y = Math.floor((Math.random()*450));
-    return y;
-}
-
-/**
- * Draws random rectangle
- */
- function randomRectangle(){
-    let rectangle = gameBoard.getContext("2d");
-    rectangle.clearRect(0,0,500,500);
-    rectangle.beginPath();
-    x = randomX();
-    y = randomY();
-    rectangle.rect(x,y,50,50);
-    rectangle.fillStyle="#000000";
-    rectangle.fill();
-    rectangle.closePath();
-}
 
 gameBoard.addEventListener('click',(event)=>{
     let mouseX = event.clientX;
     let mouseY = event.clientY;
-    if((mouseX > x && mouseX < x + 50) && (mouseY > y && mouseY < y + 50)){
+    if((mouseX > rect.x && mouseX < rect.x + 50) && (mouseY > rect.y && mouseY < rect.y + 50)){
         hits++;
         document.getElementById("score").innerHTML = hits;
-        randomRectangle();
+        //rect.randomRectangle();
+        rect = new Rectangle();
     }
     else{
         alert("You missed! Score: " + hits);
-        if (person != null) { //if logged in
+        if (player.name != null) { //if logged in
             if (hits > highScore) { //if new highscore
                 highScore = hits;
                 console.log("saving new score");
                 //set new highscore in database
-                firebase.database().ref(person).set({
+                firebase.database().ref(player.name).set({
                     score: hits,
                 });
                 document.getElementById("hScore").innerHTML = hits;
